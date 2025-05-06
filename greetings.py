b88f93164083e0e95dc48b7dd70245ee67c47495
Solom-ff <www.omkar.istalkar@gmail.com>
2025-05-06 10:45:43 +0530

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -2,17 +2,17 @@ print('This file is used to perform Cloud Computing Practical (Google Colud Cons
 def greet():
   name = input("Enter your Name ")
   print("Greetings ",name)
-  print("Using this program you can perform addition operation.\nDo you want to perform?")
-  choice = input("If yes enter yes")
+  print("Using this program you can perform addition operation.\nDo you want to perform? ")
+  choice = input("If yes enter yes ")
   choice.lower()
   if choice == 'yes':
     re=addition()
-    print("Addition of teo numbers is ",re)
+    print("Addition of two numbers is ",re)
 
 def addition():
-  print("Enter below two numbers which are required to do addition")
-  num1 = int(input("Enter first number"))
-  num2 = int(input("Enter second number"))
+  print("Enter below two numbers which are required to do addition ")
+  num1 = int(input("Enter first number "))
+  num2 = int(input("Enter second number "))
   result = num1 + num2
   return result
 
